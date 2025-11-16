f036f233d796f7d41fe93a0e3fbe62aa5157f9d3
bhavyacs1074 <b23cs1074@iitj.ac.in>
2025-11-16 02:44:33 -0800

    f1

--- ps.c
+++ ps.c
@@ -2,48 +2,20 @@
 #include "stat.h"
 #include "user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+
     int e_flag = 0;
-
-    if(argc > 1 && strcmp(argv[1], "-e") == 0)
+    if (argc > 1 && strcmp(argv[1], "-e") == 0) {
         e_flag = 1;
-
-    if(!e_flag){
-        int pid = getpid();
-
-        // set name to "ps"
-        set_proc_name(pid, "ps");
-
+    }   
+    if (!e_flag)
+    {
         printf(1, "PID\tNAME\tSTATE\tSYS\tINT\n");
-
-        printf(1, "%d\tps\t%s\t%d\t%d\n",
-            pid,
-            get_proc_state(pid),           // prints RUNNING/SLEEPING/ZOMBIE
-            get_num_syscalls(pid),
-            get_num_timer_interrupts(pid)
-        );
-
-        exit();
     }
-
-    // ps -e
-    printf(1, "PID\tSTATE\tSYS\tINT\n");
-
-    for(int pid = 1; pid <= 64; pid++){
-        int sysc = get_num_syscalls(pid);
-        int tinc = get_num_timer_interrupts(pid);
-
-        if(sysc >= 0){ // valid process
-            printf(1, "%d\t%s\t%d\t%d\n",
-                pid,
-                get_proc_state(pid),
-                sysc,
-                tinc
-            );
-        }
+    else 
+    {
+        printf(1, "PID\tSTATE\tSYS\tINT\n"); 
     }
-
     exit();
 }
