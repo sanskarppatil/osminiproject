d789c4de981fafc842759bc17173740accf476cc
Patil Sanskar <b23cs1050@iitj.ac.in>
2025-11-16 15:56:22 +0000

    Implemented cmd line args in helloyou_test.c

--- helloYou_test.c
+++ helloYou_test.c
@@ -2,11 +2,21 @@
 #include "stat.h"
 #include "user.h"
 
-int main(int argc,char *argv[])
+int
+main(int argc, char *argv[])
 {
-    for (int i = 0; i < 10; i++){
+    if(argc > 1){
+        // User gave a string as argument → pass it to syscall
+        // argv[1] is the first argument string
+        helloYou(argv[1]);
+        exit();
+    }
+
+    // No arguments → run your default loop
+    for(int i = 0; i < 10; i++){
         helloYou("Calling from XV6");
         helloYou("Welcome to XV6");
     }
+
     exit();
 }
